589e06cf7dd4de9f58752888c690d2f5664bf1b2
Maxence Dunnewind <maxence@typhon.com>
2017-10-24 17:53:15 +0200

    add index generation script

--- generate_index.sh
+++ generate_index.sh
@@ -1,18 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 
 DST="index.md"
 
 echo -e "# Rot26 WriteUps \n" > $DST
 
-for ctf in CTF/*;do 
+for ctf in CTF/*;do
+    echo "" >> $DST
     echo "## ${ctf##*/}" >> $DST
-    for year in ${ctf}/*;do 
+    for year in ${ctf}/*;do
         echo "### ${year##*/}" >> $DST
-        for cat in ${year}/*;do 
+        for cat in ${year}/*;do
             for chall in ${cat}/*;do
                 _cat=${cat##*/}
                 _chall=${chall##*/}
-                echo " * [${_cat}/${chall}]($chall)" >> $DST
+                echo " * [${_cat}/${_chall}]($chall)" >> $DST
             done
         done
     done
